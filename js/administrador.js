1143a5d86134e457e1c604c4c5023bda7f4008b5
Leandro <leoroan@gmail.com>
2024-07-05 22:29:59 -0300

    seguimos con los hotfixes

--- js/administrador.js
+++ js/administrador.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-  if (localStorage.getItem('isLoggedIn') !== 'true') {
-    document.getElementById('loginButton').style.display = 'block';
-    document.getElementById('logOutButton').style.display = 'none';
-    document.getElementById('adminButton').style.display = 'none';
-    document.getElementById('registerButton').style.display = 'bloc';
-  } else {
+  if (localStorage.getItem('isLoggedIn') === 'true') {
     document.getElementById('loginButton').style.display = 'none';
     document.getElementById('registerButton').style.display = 'none';
+    document.getElementById('adminButton').style.display = 'block';
+  } else {
+    document.getElementById('loginButton').style.display = 'block';
+    document.getElementById('logOutButton').style.display = 'none';
+    document.getElementById('registerButton').style.display = 'bloc';
   }
 });
+
 function logout() {
   var confirmation = confirm("¿Está seguro que desea cerrar sesión?");
   if (confirmation) {
     localStorage.removeItem('isLoggedIn');
     document.getElementById('loginButton').style.display = 'none';
     window.location.href = 'pages/login.html';
-
   }
+}
 
 
-  
--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-  if (localStorage.getItem('isLoggedIn') !== 'true') {
-    document.getElementById('loginButton').style.display = 'block';
-    document.getElementById('logOutButton').style.display = 'none';
-    document.getElementById('adminButton').style.display = 'none';
-    document.getElementById('registerButton').style.display = 'bloc';
-    document.getElementById('mainSectionLogged').style.display = 'none';
-  } else {
+  if (localStorage.getItem('isLoggedIn') === 'true') {
     document.getElementById('loginButton').style.display = 'none';
     document.getElementById('registerButton').style.display = 'none';
     document.getElementById('mainSection').style.display = 'none';
+  } else {
+    document.getElementById('loginButton').style.display = 'block';
+    document.getElementById('logOutButton').style.display = 'none';
+    document.getElementById('registerButton').style.display = 'bloc';
+    document.getElementById('mainSectionLogged').style.display = 'none';
+    document.getElementById('adminButton').style.display = 'none';
   }
 });
 
